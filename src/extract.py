c40092d2ff23c20cdfa5b9599986894e00c233f9
Mburns1212 <matthew.burns388@gmail.com>
2025-05-30 16:23:17 +0100

    fix layer so lambda can access utils

--- src/extract.py
+++ src/extract.py
@@ -1,10 +1,12 @@
+import sys
+sys.path.append('/opt/python')
+
 from utils.fetch_data import make_connection, close_connection, get_data
 from utils.data_to_bucket import data_to_bucket
 import boto3
 
 
 def lambda_handler(event, context):
-
     conn = make_connection()
     tables = [
         "counterparty",
@@ -24,13 +26,10 @@ def lambda_handler(event, context):
 
     s3_client = boto3.client('s3')
 
-    data_to_bucket(input_data, "ingestion-zone-bucket-20250528125730780500000003", s3_client)
+    data_to_bucket(input_data, "ingestion-zone-bucket-20250530145907229100000002", s3_client)
 
 
     # for table in tables:
     #     query = f"SELECT * FROM {table}"
     #     result = get_data(conn, query, table)
     close_connection(conn)
-
-
-lambda_handler({}, {})
