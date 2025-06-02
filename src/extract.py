69118f693425b280320cf16a6728571b8b227352
Mburns1212 <matthew.burns388@gmail.com>
2025-06-02 17:02:49 +0100

    change extract to add each table seperately

--- src/extract.py
+++ src/extract.py
@@ -3,37 +3,55 @@ sys.path.append('/opt/python')
 
 from utils.fetch_data import make_connection, close_connection, get_data
 from utils.data_to_bucket import data_to_bucket
+from utils.check_data_updates import check_data_updates
 import boto3
+import os
 
 
 def lambda_handler(event, context):
     conn = make_connection()
-    # tables = [
-    #     "counterparty",
-    #     "currency",
-    #     "department",
-    #     "design",
-    #     "staff",
-    #     "sales_order",
-    #     "address",
-    #     "payment",
-    #     "purchase_order",
-    #     "payment_type",
-    #     "transaction",
-    # ]
+    tables = [
+        "counterparty",
+        "currency",
+        "department",
+        "design",
+        "staff",
+        "sales_order",
+        "address",
+        "payment",
+        "purchase_order",
+        "payment_type",
+        "transaction",
+    ]
 
-    tables = conn.run("""SELECT * FROM information_schema.tables
-                     WHERE table_schema = 'public'
-                      AND table_name != '_prisma_migrations'""")
+    # tables = conn.run("""SELECT * FROM information_schema.tables
+    #                  WHERE table_schema = 'public'
+    #                   AND table_name != '_prisma_migrations'""")
 
     s3_client = boto3.client('s3')
-    
-    input_data = {table_name: get_data(conn, f"SELECT * FROM {table_name}", table_name)[table_name] for table_name in tables}
+    for table in tables:
+        new_data = get_data(conn, f"SELECT * FROM {table}", table)
+        if check_data_updates(new_data):
+            data_to_bucket(new_data, 'ingestion-zone-bucket-20250602144324399800000002', s3_client)
 
-    data_to_bucket(input_data, "ingestion-zone-bucket-20250530145907229100000002", s3_client)
-
-
-    # for table in tables:
-    #     query = f"SELECT * FROM {table}"
-    #     result = get_data(conn, query, table)
     close_connection(conn)
+
+#os.environ.get('BUCKET')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  # input_data = {table_name: get_data(conn, f"SELECT * FROM {table_name}", table_name)[table_name] for table_name in tables}
+
+    # data_to_bucket(input_data, "ingestion-zone-bucket-20250530145907229100000002", s3_client)
