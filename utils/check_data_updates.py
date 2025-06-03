dba7c605684e4c8e9c6684bed3b5bfb977c2cdbd
Beth Suffield <beth.suffield@gmail.com>
2025-06-03 11:52:00 +0100

    amends to lambda and its tests plus check_data_updates util

--- utils/check_data_updates.py
+++ utils/check_data_updates.py
@@ -7,15 +7,26 @@ import json
 
 def check_data_updates(new_data):
     load_dotenv()
+
     s3_client = client('s3')
+    
     table_name = list(new_data.keys())[0]
+    
     objects_response = s3_client.list_objects_v2(Bucket=os.environ.get('BUCKET'), Prefix=table_name)
     print(objects_response)
+    if objects_response['KeyCount'] == 0:
+        return True
+    
     times_list = [obj['LastModified'] for obj in objects_response['Contents']]
+    
     most_recent = max(times_list)
     most_recent_key = [obj['Key'] for obj in objects_response['Contents'] if obj['LastModified'] == most_recent][0]
+    
     current_data = s3_client.get_object(Bucket=os.environ.get('BUCKET'), Key=most_recent_key)['Body'].read().decode('utf-8')
     json_current = json.loads(current_data)
+    print("json current:", json_current)
+    print("new data:", new_data)
+    
     if json_current == new_data:
         return False
     else:
