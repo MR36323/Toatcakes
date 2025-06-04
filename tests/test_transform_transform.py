f1c2c422a545210ae88401f5e660b6f32ceff2ba
rajnijain19 <rajnijain19@gmail.com>
2025-06-04 15:08:34 +0100

    transformations

--- tests/test_transform_transform.py
+++ tests/test_transform_transform.py
@@ -15,27 +15,47 @@ import numpy as np
 
 
 class TestCreateDimStaff:
+    @pytest.fixture
+    def test_data(scope='function'):
+        return ([{'staff_id': 1, 'first_name': 'Jeremie', 'last_name': 'Franey', 'department_id': 2, 'email_address': 'jeremie.franey@terrifictotes.com', 'created_at': datetime.datetime(2022, 11, 3, 14, 20, 51, 563000), 'last_updated': datetime.datetime(2022, 11, 3, 14, 20, 51, 563000)}, {'staff_id': 2, 'first_name': 'Deron', 'last_name': 'Beier', 'department_id': 1, 'email_address': 'deron.beier@terrifictotes.com', 'created_at': datetime.datetime(2022, 11, 3, 14, 20, 51, 563000), 'last_updated': datetime.datetime(2022, 11, 3, 14, 20, 51, 563000)}],
+                [{'department_id': 1, 'department_name': 'Sales', 'location': 'Manchester', 'manager': 'Richard Roma', 'created_at': datetime.datetime(2022, 11, 3, 14, 20, 49, 962000), 'last_updated': datetime.datetime(2022, 11, 3, 14, 20, 49, 962000)}, {'department_id': 2, 'department_name': 'Purchasing', 'location': 'Manchester', 'manager': 'Naomi Lapaglia', 'created_at': datetime.datetime(2022, 11, 3, 14, 20, 49, 962000), 'last_updated': datetime.datetime(2022, 11, 3, 14, 20, 49, 962000)}])
+
+    def test_output_is_of_type_dataframe(self, test_data):
+        result_df = create_dim_staff(test_data[0],test_data[1])
+        assert type(result_df) == pandas.core.frame.DataFrame
+
+    def test_correct_clmn_names(self,test_data):
+        result_df = create_dim_staff(test_data[0],test_data[1])
+        assert set(result_df.columns.values) == {'staff_id','first_name','last_name','department_name','location','email_address'}
     
-    def test_output_is_of_type_dataframe(self):
-        ...
+    def test_correct_clmn_data_types(self,test_data):
+        result_df = create_dim_staff(test_data[0],test_data[1])
+        assert type(result_df.loc[0]['staff_id']) == np.int64
+        assert type(result_df.loc[0]['first_name']) == str
+        assert type(result_df.loc[0]['last_name']) == str
+        assert type(result_df.loc[0]['department_name']) == str
+        assert type(result_df.loc[0]['location']) == str
+        assert type(result_df.loc[0]['email_address']) == str
 
-    def test_correct_clmn_names(self):
-        ...
-    
-    def test_correct_clmn_data_types(self):
-        ...
-
-    def test_correct_data_values(self):
-        ...
-
+    def test_correct_data_values(self, test_data):
+        result_df = create_dim_staff(test_data[0],test_data[1])
+        row_list_0 = result_df.loc[0, :].values.tolist()
+        row_list_1 = result_df.loc[1, :].values.tolist()
+        assert row_list_0 == [1,'Jeremie', 'Franey','jeremie.franey@terrifictotes.com','Purchasing','Manchester']
+        assert row_list_1 == [2, 'Deron', 'Beier', 'deron.beier@terrifictotes.com','Sales','Manchester']
 
 class TestCreateDimCounterparty:
-    
-    def test_output_is_of_type_dataframe(self):
-        ...
+    @pytest.fixture
+    def test_data(scope='function'):
+        return ([{'counterparty_id': 1, 'counterparty_legal_name': 'Fahey and Sons', 'legal_address_id': 15, 'commercial_contact': 'Micheal Toy', 'delivery_contact': 'Mrs. Lucy Runolfsdottir', 'created_at': datetime.datetime(2022, 11, 3, 14, 20, 51, 563000), 'last_updated': datetime.datetime(2022, 11, 3, 14, 20, 51, 563000)}],
+                [{'address_id':15,'address_line_1':'test_line_1','address_line_2':'test_line_2','district':'test_district','city':'test_city','postal_code':'test_code','country':'test_country','phone':'test_phone','created_at':datetime.datetime(2022, 11, 3, 14, 20, 51, 563000),'last_updated': datetime.datetime(2022, 11, 3, 14, 20, 51, 563000)}])
+    def test_output_is_of_type_dataframe(self,test_data):
+        result_df = create_dim_counterparty(test_data[0],test_data[1])
+        assert type(result_df) == pandas.core.frame.DataFrame
 
     def test_correct_clmn_names(self):
-        ...
+        result_df = create_dim_counterparty(test_data)
+        assert set(result_df.columns.values) == {'design_id', 'design_name', 'file_location', 'file_name'}
 
     def test_correct_clmn_data_types(self):
         ...
@@ -67,7 +87,7 @@ class TestCreateDimDesign:
 
     def test_correct_clmn_names(self, test_data):
         result_df = create_dim_design(test_data)
-        assert list(result_df.columns.values) == ['design_id', 'design_name', 'file_location', 'file_name']
+        assert set(result_df.columns.values) == {'design_id', 'design_name', 'file_location', 'file_name'}
 
     def test_correct_clmn_data_types(self, test_data):
         result_df = create_dim_design(test_data)
@@ -95,7 +115,7 @@ class TestCreateDimLocation:
 
     def test_correct_clmn_names(self, test_data):
         result_df = create_dim_location(test_data)
-        assert list(result_df.columns.values) == ['location_id', 'address_line_1', 'address_line_2', 'district', 'city', 'postal_code', 'country', 'phone']
+        assert set(result_df.columns.values) == {'location_id', 'address_line_1', 'address_line_2', 'district', 'city', 'postal_code', 'country', 'phone'}
 
     def test_correct_clmn_data_types(self, test_data):
         result_df = create_dim_location(test_data)
--- utils/transform_transform.py
+++ utils/transform_transform.py
@@ -18,7 +18,7 @@ NOTE: to track latest sales record id, it might be easiest to store
       when it is time to increment the sales records id.
 '''
 
-def create_dim_staff(staff: str, department: str) -> pd.DataFrame:
+def create_dim_staff(staff: list, department: list) -> pd.DataFrame:
     """Create and populate new dimension staff table.
 
     Args:
@@ -28,9 +28,13 @@ def create_dim_staff(staff: str, department: str) -> pd.DataFrame:
     Returns:
       Pandas DataFrame object representing a staff dimension table.
     """
-    pass
+    staff_df = pd.DataFrame(staff)
+    department_df = pd.DataFrame(department)
+    dim_staff_df = pd.merge(staff_df,department_df,on='department_id')
+    dim_staff_df = dim_staff_df.drop(['department_id','manager','created_at_x','last_updated_y','created_at_y','last_updated_x'],axis=1)
+    return dim_staff_df
 
-def create_dim_counterparty(counterparty: str, address: str) -> pd.DataFrame:
+def create_dim_counterparty(counterparty: list, address: list) -> pd.DataFrame:
     """Create and populate new dimension counterparty table.
 
     Args:
@@ -40,7 +44,11 @@ def create_dim_counterparty(counterparty: str, address: str) -> pd.DataFrame:
     Returns:
       Pandas DataFrame object representing a counterparty dimension table.
     """
-    pass
+    counterparty_df = pd.DataFrame(counterparty)
+    address_df = pd.DataFrame(address)
+    dim_counterparty_df = pd.merge(counterparty_df,address_df,left_on='legal_address_id',right_on='address_id')
+    dim_counterparty_df = dim_counterparty_df.drop(['address_id','legal_address_id','commercial_contact','delivery_contact','created_at_x','last_updated_y','created_at_y','last_updated_x',],axis=1)
+    print(dim_counterparty_df)
 
 def create_dim_currency(currency: str) -> pd.DataFrame:
     """Create and populate new dimension currency table.
@@ -66,7 +74,7 @@ def create_dim_design(design: list) -> pd.DataFrame:
     dim_design_df = design_df.drop(['created_at', 'last_updated'], axis=1)
     return dim_design_df
 
-def create_dim_location(address: str) -> pd.DataFrame:
+def create_dim_location(address: list) -> pd.DataFrame:
     """Create and populate new dimension address table.
 
     Args:
