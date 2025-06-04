bb824a8ae62f5820fae95d6696f7ce2051ff5800
lathiuu <heilalok02@gmail.com>
2025-06-04 14:12:45 +0100

    add testing and solution for create_dim_currency function

--- tests/test_transform_transform.py
+++ tests/test_transform_transform.py
@@ -7,129 +7,144 @@ from utils.transform_transform import (
     create_dim_date, 
     create_fact_sales_order
 )
+import json
+import pandas as pd
 
 
-class TestCreateDimStaff:
+# class TestCreateDimStaff:
     
-    def test_output_is_of_type_dataframe(self):
-        ...
+#     def test_output_is_of_type_dataframe(self):
+#         ...
 
-    def test_correct_clmn_names(self):
-        ...
+#     def test_correct_clmn_names(self):
+#         ...
     
-    def test_correct_clmn_data_types(self):
-        ...
+#     def test_correct_clmn_data_types(self):
+#         ...
 
-    def test_correct_data_values(self):
-        ...
+#     def test_correct_data_values(self):
+#         ...
 
 
-class TestCreateDimCounterparty:
+# class TestCreateDimCounterparty:
     
-    def test_output_is_of_type_dataframe(self):
-        ...
+#     def test_output_is_of_type_dataframe(self):
+#         ...
 
-    def test_correct_clmn_names(self):
-        ...
+#     def test_correct_clmn_names(self):
+#         ...
 
-    def test_correct_clmn_data_types(self):
-        ...
+#     def test_correct_clmn_data_types(self):
+#         ...
 
-    def test_correct_data_values(self):
-        ...
+#     def test_correct_data_values(self):
+#         ...
 
 
 class TestCreateDimCurrency:
     
     def test_output_is_of_type_dataframe(self):
-        ...
 
-    def test_correct_clmn_names(self):
-        ...
+        test_input = [{"currency_id": 1, "currency_code": "GBP", "created_at": "2022-11-03 14:20:49.962000", "last_updated": "2022-11-03 14:20:49.962000"}, {"currency_id": 2, "currency_code": "USD", "created_at": "2022-11-03 14:20:49.962000", "last_updated": "2022-11-03 14:20:49.962000"}, {"currency_id": 3, "currency_code": "EUR", "created_at": "2022-11-03 14:20:49.962000", "last_updated": "2022-11-03 14:20:49.962000"}]
 
-    def test_correct_clmn_data_types(self):
-        ...
+        response = create_dim_currency(test_input)
+        assert isinstance(response, pd.DataFrame)
+
+    def test_correct_clmn_names_are_returned(self):
+        
+        test_input = [{"currency_id": 1, "currency_code": "GBP", "created_at": "2022-11-03 14:20:49.962000", "last_updated": "2022-11-03 14:20:49.962000"}, {"currency_id": 2, "currency_code": "USD", "created_at": "2022-11-03 14:20:49.962000", "last_updated": "2022-11-03 14:20:49.962000"}, {"currency_id": 3, "currency_code": "EUR", "created_at": "2022-11-03 14:20:49.962000", "last_updated": "2022-11-03 14:20:49.962000"}]
+
+        response = create_dim_currency(test_input)
+        assert list(response.columns) == ['currency_id', 'currency_code', 'currency_name']
+
+    def test_handles_currency_name_not_in_dictionary(self):
+
+        test_input = [{"currency_id": 1, "currency_code": "JPY", "created_at": "2022-11-03 14:20:49.962000", "last_updated": "2022-11-03 14:20:49.962000"}, {"currency_id": 2, "currency_code": "EUR", "created_at": "2022-11-03 14:20:49.962000", "last_updated": "2022-11-03 14:20:49.962000"}]
+
+        response = create_dim_currency(test_input)
+        print(list(response["currency_name"]))
+        assert list(response["currency_name"]) == ["Unknown", "Euro"]
 
 
-class TestCreateDimDesign:
+# class TestCreateDimDesign:
     
-    def test_output_is_of_type_dataframe(self):
-        ...
+#     def test_output_is_of_type_dataframe(self):
+#         ...
 
-    def test_correct_clmn_names(self):
-        ...
+#     def test_correct_clmn_names(self):
+#         ...
 
-    def test_correct_clmn_data_types(self):
-        ...
+#     def test_correct_clmn_data_types(self):
+#         ...
 
-    def test_correct_data_values(self):
-        ...
+#     def test_correct_data_values(self):
+#         ...
 
 
-class TestCreateDimLocation:
+# class TestCreateDimLocation:
     
-    def test_output_is_of_type_dataframe(self):
-        ...
+#     def test_output_is_of_type_dataframe(self):
+#         ...
 
-    def test_correct_clmn_names(self):
-        ...
+#     def test_correct_clmn_names(self):
+#         ...
 
-    def test_correct_clmn_data_types(self):
-        ...
+#     def test_correct_clmn_data_types(self):
+#         ...
 
-    def test_correct_data_values(self):
-        ...
+#     def test_correct_data_values(self):
+#         ...
     
 
-class TestCreateDimDate:
+# class TestCreateDimDate:
     
-    def test_output_is_of_type_dataframe(self):
-        ...
+#     def test_output_is_of_type_dataframe(self):
+#         ...
 
-    def test_correct_clmn_names(self):
-        ...
+#     def test_correct_clmn_names(self):
+#         ...
 
-    def test_correct_clmn_data_types(self):
-        ...
+#     def test_correct_clmn_data_types(self):
+#         ...
 
-    def test_correct_data_values(self):
-        ...    
+#     def test_correct_data_values(self):
+#         ...    
 
 
-class TestCreateFactSalesOrder:
+# class TestCreateFactSalesOrder:
     
-    def test_output_is_of_type_dataframe(self):
-        ...
+#     def test_output_is_of_type_dataframe(self):
+#         ...
 
-    def test_correct_clmn_names(self):
-        ...
+#     def test_correct_clmn_names(self):
+#         ...
 
-    def test_correct_clmn_data_types(self):
-        ...
+#     def test_correct_clmn_data_types(self):
+#         ...
 
-    def test_correct_data_values(self):
-        ...
+#     def test_correct_data_values(self):
+#         ...
 
-    def test_new_sales_record_id_given_new_sales_order_id(self):
-        ...
+#     def test_new_sales_record_id_given_new_sales_order_id(self):
+#         ...
 
-    def test_new_sales_record_id_given_update_to_record_with_same_sales_order_id(self):
-        ...
-
-
-class TestLatestGetSalesRecordId:
-
-    def test_gets_latest_record_id_if_already_present(self):
-        ...
-
-    def test_raises_exception_if_record_id_not_already_present(self):
-        ...
+#     def test_new_sales_record_id_given_update_to_record_with_same_sales_order_id(self):
+#         ...
 
 
-class TestUpdateLatestSalesRecordId:
+# class TestLatestGetSalesRecordId:
 
-    def test_update_latest_record_id_if_already_present(self):
-        ...
+#     def test_gets_latest_record_id_if_already_present(self):
+#         ...
 
-    def test_update_latest_record_id_if_not_already_present(self):
-        ...
+#     def test_raises_exception_if_record_id_not_already_present(self):
+#         ...
+
+
+# class TestUpdateLatestSalesRecordId:
+
+#     def test_update_latest_record_id_if_already_present(self):
+#         ...
+
+#     def test_update_latest_record_id_if_not_already_present(self):
+#         ...
--- utils/transform_transform.py
+++ utils/transform_transform.py
@@ -18,79 +18,94 @@ NOTE: to track latest sales record id, it might be easiest to store
       when it is time to increment the sales records id.
 '''
 
-def create_dim_staff(staff: str, department: str) -> pd.DataFrame:
+def create_dim_staff(staff: list, department: list) -> pd.DataFrame:
     """Create and populate new dimension staff table.
 
     Args:
-      staff: Staff table in the form of a json string.
-      department: Department table in the form of a json string.
+      staff: Staff table in the form of a json listing.
+      department: Department table in the form of a json listing.
 
     Returns:
       Pandas DataFrame object representing a staff dimension table.
     """
     pass
 
-def create_dim_counterparty(counterparty: str, address: str) -> pd.DataFrame:
+def create_dim_counterparty(counterparty: list, address: list) -> pd.DataFrame:
     """Create and populate new dimension counterparty table.
 
     Args:
-      counterparty: Counterparty table in the form of a json string.
-      address: Address table in the form of a json string.
+      counterparty: Counterparty table in the form of a json listing.
+      address: Address table in the form of a json listing.
 
     Returns:
       Pandas DataFrame object representing a counterparty dimension table.
     """
     pass
 
-def create_dim_currency(currency: str) -> pd.DataFrame:
+def create_dim_currency(currency: list) -> pd.DataFrame:
     """Create and populate new dimension currency table.
 
     Args:
-      currency: Currency table in the form of a json string.
+      currency: Currency table in the form of a json listing.
 
     Returns:
       Pandas DataFrame object representing a currency dimension table.
     """
-    pass
 
-def create_dim_design(design: str) -> pd.DataFrame:
+    currency_name = {
+        "GBP": "British pound sterling",
+        "USD":  "United States dollar",
+        "EUR":  "Euro"
+    }
+
+    dim_currency_df = pd.DataFrame(currency)
+    dim_currency_df = dim_currency_df.drop(['created_at', 'last_updated'], axis=1)
+
+    dim_currency_df['currency_name'] = dim_currency_df['currency_code'].map(currency_name)
+    dim_currency_df = dim_currency_df.fillna("Unknown")
+    
+    return dim_currency_df
+
+
+
+def create_dim_design(design: list) -> pd.DataFrame:
     """Create and populate new dimension design table.
 
     Args:
-      Design: Design table in the form of a json string.
+      Design: Design table in the form of a json listing.
 
     Returns:
       Pandas DataFrame object representing a design dimension table.
     """
     pass
 
-def create_dim_location(address: str) -> pd.DataFrame:
+def create_dim_location(address: list) -> pd.DataFrame:
     """Create and populate new dimension address table.
 
     Args:
-      Address: Address table in the form of a json string.
+      Address: Address table in the form of a json listing.
 
     Returns:
       Pandas DataFrame object representing a location dimension table.
     """
     pass
 
-def create_dim_date(sale_order: str) -> pd.DataFrame:
+def create_dim_date(sale_order: list) -> pd.DataFrame:
     """Create and populate new dimension date table.
 
     Args:
-      Sale_order: Sale_order table in the form of a json string.
+      Sale_order: Sale_order table in the form of a json listing.
 
     Returns:
       Pandas DataFrame object representing a sale_order dimension table.
     """
     pass
 
-def create_fact_sales_order(sales_order: str) -> pd.DataFrame:
+def create_fact_sales_order(sales_order: list) -> pd.DataFrame:
     """Create and populate new fact sales_order table.
 
     Args:
-      sales_order: Sales_order table in the form of a json string.
+      sales_order: Sales_order table in the form of a json listing.
 
     Returns:
       Pandas DataFrame object representing a fact sales_order table.
