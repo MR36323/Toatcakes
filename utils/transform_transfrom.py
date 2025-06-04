1bb0b224fe81e1f81d427403f82a2defa007d299
lathiuu <heilalok02@gmail.com>
2025-06-04 09:31:45 +0100

    add minor updates to function docstrings

--- utils/transform_transfrom.py
+++ utils/transform_transfrom.py
@@ -8,14 +8,6 @@ dim_design -> design
 dim_location -> address
 dim_date -> sales_order
 fact_sales_order -> sales_order
-
-
-NOTE: to track latest sales record id, it might be easiest to store
-      the id as a seperate object in the processed zone bucket.
-      We can then decide what to do with the tables in the bucket 
-      each time we run the pipeline. But we do NOT clear the id each time we
-      run the pipeline; only get_latest_sales_record_id() may change this value,
-      when it is time to increment the sales records id.
 '''
 
 def create_dim_staff(staff: str, department: str) -> pd.DataFrame:
@@ -76,7 +68,7 @@ def create_dim_location(address: str) -> pd.DataFrame:
     pass
 
 def create_dim_date(sale_order: str) -> pd.DataFrame:
-    """Create and populate new sale_order table.
+    """Create and populate new sale_order address table.
 
     Args:
       Sale_order: Sale_order table in the form of a json string.
@@ -87,7 +79,7 @@ def create_dim_date(sale_order: str) -> pd.DataFrame:
     pass
 
 def create_fact_sales_order(sales_order: str) -> pd.DataFrame:
-    """Create and populate new fact sales_order table.
+    """Create and populate new sales_order address table.
 
     Args:
       sales_order: Sales_order table in the form of a json string.
@@ -96,27 +88,3 @@ def create_fact_sales_order(sales_order: str) -> pd.DataFrame:
       Pandas DataFrame object representing a fact sales_order table.
     """
     pass
-
-def get_latest_sales_record_id() -> int:
-    """Get the latest sales record id from processed zone bucket.
-
-    Args: 
-      None.
-
-    Returns:
-      Integer representing the latest sales_record_id.
-
-    Raises:
-      Exception.
-    """
-
-def update_latest_sales_record_id(sales_record_id: int):
-    """Put the latest sales record id into processed zone bucket.
-
-    Args: 
-      sales_record_id: The latest sales record id.
-
-    Returns:
-      None.
-    """
-
